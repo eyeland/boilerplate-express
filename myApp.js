e2cffe35cbcd0cfbb6357663d060b5fb04419cf8
Bryant <63192606+eyeland@users.noreply.github.com>
2022-05-30 22:19:02 +0000

    redirect users from path to form

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let app = express();
+console.log('Hello World')
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath)
 });
 
-console.log('Hello World')
+
 
 
 
